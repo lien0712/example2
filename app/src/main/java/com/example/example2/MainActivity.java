a147fb13be951b9b101c72f8351de88483e7f147
Vu Lien <vuthilien712@gmail.com>
2022-12-30 09:52:39 +0700

    1

--- app/src/main/java/com/example/example2/MainActivity.java
+++ app/src/main/java/com/example/example2/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.example2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnCount ;
+    TextView txtCount;
+    int mCount=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCount=findViewById(R.id.button_count);
+        txtCount= findViewById(R.id.show_count);
+        btnCount.setOnClickListener(view -> {
+            //Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT).show();
+            //txtCount.setText("12");
+            mCount++;
+            txtCount.setText(""+mCount);
+        });
+
+    }
+
+    public void showToast(View view) {
+        mCount=0;
+        txtCount.setText("0");
+
     }
 }
